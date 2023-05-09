dab1da3fbd967649d4780f42584f3a18760c50c7
kazuki1023 <k.keio12569@gmail.com>
2023-05-09 16:47:25 +0900

    tailwind使えるようになった

--- resources/views/movies/register.blade.php
+++ resources/views/movies/register.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
+  @component('components.link')
+  @endcomponent
+  <title>管理者画面</title>
 </head>
 <body>
-  
+  @component('components.header')
+  @endcomponent
 </body>
 </html>
