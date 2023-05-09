34dc1610b9d7daf5a721836ad3dae19050d0a957
kazuki1023 <k.keio12569@gmail.com>
2023-05-09 18:45:15 +0900

    登録画面のフロント完成

--- resources/views/movies/register.blade.php
+++ resources/views/movies/register.blade.php
@@ -1,28 +1,66 @@
 @extends('layouts.index')
 
 @section('content')
-  <form action="/" method="POST">
-    @csrf
-    <div class="form-group">
-      <label for="title">タイトル</label>
-      <input type="text" name="title" id="title">
-    </div>
-    <div class="form-group">
-      <label for="image_url">画像URL</label>
-      <input type="text" name="image_url" id="image_url">
-    </div>
-    <div class="form-group">
-      <label for="published_year">公開年</label>
-      <input type="text" name="published_year" id="published_year">
-    </div>
-    <div class="form-group">
-      <label for="description">概要</label>
-      <textarea name="description" id="description" cols="30" rows="10"></textarea>
-    </div>
-    <div class="form-group">
-      <label for="is_showing">上映中かどうか</label>
-      <input type="checkbox" name="is_showing" id="is_showing">
-    </div>
-    <input type="submit" value="登録">
-  </form>
+    <form action="/admin/movies/store" method="POST" class="">
+        @csrf
+        <div class="mb-6 w-4/5">
+            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">タイトル</label>
+            <input type="text" id="title"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="title" required>
+        </div>
+        <div class="mb-6 w-4/5">
+            <label for="image_url" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">画像URL</label>
+            <input type="text" id="image_url"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="image_url" required>
+        </div>
+        <div class="mb-6 w-4/5">
+            <label for="published_year" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">公開年</label>
+            <input type="text" id="published_year"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="published_year" required>
+        </div>
+        <div class="w-4/5 mb-6">
+            <label for="is_showing" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">公開中かどうか</label>
+            <select id="is_showing"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" name="is_showing" required>
+                <option selected>選んでください</option>
+                <option value="true">公開中</option>
+                <option value="false">公開中止</option>
+            </select>
+        </div>
+        <div class="w-4/5 mb-6">
+            <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">概要</label>
+            <textarea id="description" rows="4"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write your thoughts here..." name="description" required></textarea>
+        </div>
+        <div class="relative max-w-sm mb-6">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400 mt-5" fill="currentColor"
+                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </div>
+            <label for="created_at">登録日時</label>
+            <input datepicker datepicker-autohide type="text"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Select date" id="created_at" name="created_at" required>
+        </div>
+        <div class="relative max-w-sm mb-6">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400 mt-5" fill="currentColor"
+                    viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd"
+                        d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
+                        clip-rule="evenodd"></path>
+                </svg>
+            </div>
+            <label for="updated_at">更新日時</label>
+            <input datepicker datepicker-autohide type="text"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Select date" id="updated_at" name="updated_at" required>
+        </div>
+
+    </form>
 @endsection
