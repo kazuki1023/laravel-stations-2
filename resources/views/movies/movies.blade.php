f37e864a631e1b24c7cd306a98c4db703dfd40fd
kazuki1023 <k.keio12569@gmail.com>
2023-07-19 17:34:04 +0900

    fix デザイン修正

--- resources/views/movies/movies.blade.php
+++ resources/views/movies/movies.blade.php
@@ -13,7 +13,7 @@
         <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
         <div class="relative">
             <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400"  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                 </svg>
             </div>
@@ -105,11 +105,11 @@
                         </td>
                         @if ($movie->is_showing)
                             <td class="px-6 py-4">
-                                公開中
+                                上映中
                             </td>
                         @else
                             <td class="px-6 py-4">
-                                公開予定
+                                上映予定
                             </td>
                         @endif
                         <td class="px-6 py-4">
