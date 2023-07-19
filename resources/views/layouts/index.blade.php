dbe50637bd20c6a0fe2af058bdf6e01574c9105f
kazuki1023 <k.keio12569@gmail.com>
2023-07-19 13:39:36 +0900

    add クリック時に確認ログ出現

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -13,9 +13,21 @@
     @endcomponent
     {{-- @component('components.sidebar')
     @endcomponent --}}
-    <main class="mt-14 ml-64">
+    <main class="mt-14 ml-8">
         @yield('content')
     </main>
+    <script>
+        // 削除ボタンがクリックされたときの処理
+        document.querySelectorAll('.delete-form').forEach(form => {
+            form.addEventListener('submit', function(event) {
+                event.preventDefault(); // デフォルトのフォーム送信をキャンセル
+                // 確認ダイアログを表示し、OKボタンがクリックされた場合に削除処理を実行
+                if (confirm('本当に削除しますか？')) {
+                    this.submit(); // フォームを送信
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
